7a14d514c930997e4ed050faf4803b994d62f1d7
System Administrator <root@Samarths-MacBook-Pro.local>
2019-11-12 18:24:31 -0500

    Add person model

---
+++ src/main/java/com/example/User/API/model/Person.java
@@ -0,0 +1,28 @@
+package com.example.User.API.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.UUID;
+
+import javax.validation.constraints.NotBlank;
+
+public class Person {
+
+  private final UUID id;
+
+  @NotBlank
+  private final String name;
+
+  public Person(@JsonProperty("id") UUID id, @JsonProperty("name") String name){
+    this.id = id;
+    this.name = name;
+  }
+
+  public UUID getId() {
+    return id;
+  }
+
+  public String getName() {
+    return name;
+  }
+}
